8e5945ccee0017d8ad9204945fe3e040dd44af53
NiyasAhamedK <koonariniyas@gmail.com>
2026-01-21 21:10:14 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'NiyasAhamedK',
+    author_email= 'koonariniyas@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
+
+
